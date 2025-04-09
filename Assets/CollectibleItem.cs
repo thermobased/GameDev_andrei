cd32e4f1fbc73c19926c8d5a63d32ec54a820265
unknown <oleinikov.2000.ao@gmail.com>
2025-04-09 20:57:02 +0500

    added some shrooms and hoes

--- Assets/CollectibleItem.cs
+++ Assets/CollectibleItem.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 public class CollectibleItem : MonoBehaviour
 {
     private static int keyCount = 0;
-    private TextMeshProUGUI cntComponent;
+    private static int shroomCount = 0;
+    private static int hoeCount = 0;
+    private TextMeshProUGUI keyCountText;
+    private TextMeshProUGUI hoeCountText;
+    private TextMeshProUGUI shroomCountText;
 
     void Start()
     {
-        cntComponent = GameObject.Find("KeysAmount").GetComponent<TextMeshProUGUI>();
+        keyCountText = GameObject.Find("KeysAmount").GetComponent<TextMeshProUGUI>();
+        hoeCountText = GameObject.Find("HoesAmount").GetComponent<TextMeshProUGUI>();
+        shroomCountText = GameObject.Find("ShroomsAmount").GetComponent<TextMeshProUGUI>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            keyCount++;
-            Debug.Log(keyCount);
+            switch (gameObject.name)
+            {
+                case "Hoe":
+                    hoeCount++;
+                    break;
+                case "Shroom":
+                    shroomCount++;
+                    break;
+                case "Key":
+                    keyCount++;
+                    break;
+            }
+            
+            //Debug.Log($"{keyCount}, {hoeCount}, {shroomCount}");
             UpdateCounterText();
             Destroy(gameObject);
         }
     }    
     private void UpdateCounterText()
     {
-                 cntComponent.text = $"x {keyCount.ToString()}";
+        keyCountText.text = $"x {keyCount.ToString()}";
+        shroomCountText.text = $"x {shroomCount.ToString()}";
+        hoeCountText.text = $"x {hoeCount.ToString()}";
     }
 
 
